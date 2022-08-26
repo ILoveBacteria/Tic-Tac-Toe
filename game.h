c9c48814b3d5a70683fd8affdf4280aeda9afed5
Moein Arabi <moein.mo81@gmail.com>
2022-08-26 16:53:00 +0430

    Added properties to class

--- game.h
+++ game.h
@@ -1,9 +1,46 @@
 #ifndef GAME_H
 #define GAME_H
+#define SIZE 3
 
 class Game {
 public:
+    char** gameBoard;
+    char turn = 'X';
 
+    Game() {
+        initGameBoard();
+    }
+
+    void chooseAndChangeTurn(int row, int column) {
+        choose(row, column);
+        changeTurn();
+    }
+
+private:
+    void changeTurn() {
+        if (turn == 'X') {
+            turn = 'O';
+        } else {
+            turn = 'X';
+        }
+    }
+
+    void choose(int row, int column) {
+        gameBoard[row][column] = turn;
+    }
+
+    void initGameBoard() {
+        gameBoard = new char*[SIZE];
+        for (int i = 0; i < SIZE; ++i) {
+            gameBoard[i] = new char[SIZE];
+        }
+
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                gameBoard[i][j] = '\0';
+            }
+        }
+    }
 };
 
 #endif // GAME_H
