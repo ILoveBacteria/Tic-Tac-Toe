f5f045eb85719cac8190da3d81bd380dd77d39ab
Moein Arabi <moein.mo81@gmail.com>
2022-08-26 16:50:38 +0430

    Improved ui for X/O

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -8,13 +8,59 @@ GameWindow::GameWindow(QWidget *parent) :
     ui->setupUi(this);
 }
 
-GameWindow::~GameWindow()
-{
+GameWindow::~GameWindow() {
     delete ui;
 }
 
-void GameWindow::on_pushButton_clicked()
-{
-    ui->pushButton->setText("pressed!");
+void GameWindow::on_button00_clicked() {
+    ui -> button00 -> setText(QString(game -> turn));
+    game -> chooseAndChangeTurn(0, 0);
 }
 
+
+void GameWindow::on_button01_clicked() {
+    ui -> button01 -> setText(QString(game -> turn));
+    game -> chooseAndChangeTurn(0, 1);
+}
+
+
+void GameWindow::on_button02_clicked() {
+    ui -> button02 -> setText(QString(game -> turn));
+    game -> chooseAndChangeTurn(0, 2);
+}
+
+
+void GameWindow::on_button10_clicked() {
+    ui -> button10 -> setText(QString(game -> turn));
+    game -> chooseAndChangeTurn(1, 0);
+}
+
+
+void GameWindow::on_button11_clicked() {
+    ui -> button11 -> setText(QString(game -> turn));
+    game -> chooseAndChangeTurn(1, 1);
+}
+
+
+void GameWindow::on_button12_clicked() {
+    ui -> button12 -> setText(QString(game -> turn));
+    game -> chooseAndChangeTurn(1, 2);
+}
+
+
+void GameWindow::on_button20_clicked() {
+    ui -> button20 -> setText(QString(game -> turn));
+    game -> chooseAndChangeTurn(2, 0);
+}
+
+
+void GameWindow::on_button21_clicked() {
+    ui -> button21 -> setText(QString(game -> turn));
+    game -> chooseAndChangeTurn(2, 1);
+}
+
+
+void GameWindow::on_button22_clicked() {
+    ui -> button22 -> setText(QString(game -> turn));
+    game -> chooseAndChangeTurn(2, 2);
+}
--- gamewindow.h
+++ gamewindow.h
@@ -2,6 +2,7 @@
 #define GAMEWINDOW_H
 
 #include <QWidget>
+#include "game.h"
 
 namespace Ui {
 class GameWindow;
@@ -16,10 +17,27 @@ public:
     ~GameWindow();
 
 private slots:
-    void on_pushButton_clicked();
+    void on_button00_clicked();
+
+    void on_button01_clicked();
+
+    void on_button02_clicked();
+
+    void on_button10_clicked();
+
+    void on_button11_clicked();
+
+    void on_button12_clicked();
+
+    void on_button20_clicked();
+
+    void on_button21_clicked();
+
+    void on_button22_clicked();
 
 private:
     Ui::GameWindow *ui;
+    Game *game = new Game();
 };
 
 #endif // GAMEWINDOW_H
