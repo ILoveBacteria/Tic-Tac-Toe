51b0b105f38401a68bfbefde21a171a7491fbe09
Moein Arabi <moein.mo81@gmail.com>
2022-08-27 17:37:06 +0430

    Checking the winning

--- game.h
+++ game.h
@@ -16,6 +16,57 @@ public:
         changeTurn();
     }
 
+    char checkWin() {
+        // Check rows
+        for (int i = 0; i < SIZE; i++) {
+            char temp = gameBoard[i][0];
+            bool continueOuter = false;
+
+            for (int j = 0; j < SIZE; j++) {
+                if (gameBoard[i][j] != temp) {
+                    continueOuter = true;
+                    break;
+                }
+            }
+
+            if (continueOuter) {
+                continue;
+            } else {
+                return temp;
+            }
+        }
+
+        // Check column
+        for (int i = 0; i < SIZE; i++) {
+            char temp = gameBoard[0][i];
+            bool continueOuter = false;
+
+            for (int j = 0; j < SIZE; j++) {
+                if (gameBoard[j][i] != temp) {
+                    continueOuter = true;
+                    break;
+                }
+            }
+
+            if (continueOuter) {
+                continue;
+            } else {
+                return temp;
+            }
+        }
+
+        // Check diagonal
+        if (gameBoard[0][0] == gameBoard[1][1] && gameBoard[0][0] == gameBoard[2][2]) {
+            return gameBoard[0][0];
+        }
+
+        if (gameBoard[0][2] == gameBoard[1][1] && gameBoard[0][2] == gameBoard[2][0]) {
+            return gameBoard[0][2];
+        }
+
+        return '\0';
+    }
+
 private:
     void changeTurn() {
         if (turn == 'X') {
